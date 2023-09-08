1dc919ee17fc87f7a0508119bc8e39b4e3c61425
alemr <ale.mr0214@gmail.com>
2023-09-07 22:26:02 -0600

    Building a pagination

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -189,6 +189,31 @@ function PokemonList() {
           </div>
         </div>
       )}
+      {/* Pagination */}
+      <nav aria-label="Pagination">
+        <ul className="pagination justify-content-center">
+          <li className={`page-item ${currentPage === 1 ? "disabled" : ""}`}>
+            <button
+              className="page-link"
+              onClick={() => handlePageChange(currentPage - 1)}
+            >
+              Anterior
+            </button>
+          </li>
+          <li
+            className={`page-item ${
+              currentPage === totalPages ? "disabled" : ""
+            }`}
+          >
+            <button
+              className="page-link"
+              onClick={() => handlePageChange(currentPage + 1)}
+            >
+              Siguiente
+            </button>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
